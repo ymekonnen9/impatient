b15d6432d3d872431a971f5f4d9f6020f81067d2
Yared Mekonnen <yaredmekonnen999@gmail.com>
2025-03-09 00:05:48 -0600

    generics

---
+++ generics/ClassPredicate.java
@@ -0,0 +1,22 @@
+package generics;
+import lambdaexpressions.*;
+
+public class ClassPredicate {
+  public static void printAll(Employee[] Objects, Predicate<Employee> filter){
+      for (Employee objects2 : Objects) {
+        if(filter.test(objects2)){
+          System.out.println(objects2.getFirstName());
+        };
+      }
+  }
+
+  public static void main(String[] args) {
+    Employee[] employees = {
+      new Employee("Alice", "Smith", 50000),
+      new Employee("Bob", "Johnson", 60000),
+      new Employee("Charlie", "Brown", 55000)
+  };
+  ClassPredicate.printAll(employees, (e) -> e.getMeasure() >= 1000);
+
+  }
+}
---
+++ generics/Predicate.java
@@ -0,0 +1,5 @@
+package generics;
+
+public interface Predicate <T> {
+  public boolean test(T t);
+}
